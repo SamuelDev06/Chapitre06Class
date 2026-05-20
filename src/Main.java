f6ad1b2a4a0dc48de62ef71c4a9df9434e78f35f
samuel <ogokonle@gmail.com>
2026-05-20 15:55:00 +0100

    Ajout de contact .

--- src/Main.java
+++ src/Main.java
@@ -38,6 +38,46 @@ public class Main {
 
 
         }
+        //Instanciation des classes contactsManager et Contact
+        ContactsManager myContactsManager = new ContactsManager();
+        Contact myContact = new Contact() ;
+
+        //Processus d'ajout de contact
+
+        myContact.name ="Issa" ;
+        myContact.phoneNumber ="0710480407";
+        myContactsManager.addContact(myContact);
+
+        //Reprise du processus pour 4 Contacts
+
+        Contact contact2 = new Contact() ;
+        contact2.name = "ALLANI" ;
+        contact2.phoneNumber = "0712536534" ;
+        myContactsManager.addContact(contact2);
+
+        Contact contact3 = new Contact() ;
+        contact3.name = "BROU" ;
+        contact3.phoneNumber = "0712536534" ;
+        myContactsManager.addContact(contact3);
+
+        Contact contact4 = new Contact() ;
+        contact4.name = "KOUAKOU" ;
+        contact4.phoneNumber = "0797135915" ;
+        myContactsManager.addContact(contact4);
+
+        Contact contact5 = new Contact() ;
+        contact5.name = "BAH" ;
+        contact5.phoneNumber = "0595347885";
+        myContactsManager.addContact( contact5);
+
+        //Affichage du numéro de téléphone correspondant au nom recherché
+        System.out.println("le numéro correspondant au contact recherché est :" +   myContactsManager.searchContact("ALLANI") );
+
+
+
+
+
+
 
 
     }
